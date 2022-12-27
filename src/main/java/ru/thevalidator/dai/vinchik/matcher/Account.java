d9be3658e92d3c4583bd1c13e6da9c1ddfdcc3d9
thevalidator <the.validator@yandex.ru>
2022-12-27 15:12:52 +0300

    Initial commit

---
+++ src/main/java/ru/thevalidator/dai/vinchik/matcher/Account.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2022 thevalidator
+ */
+
+package ru.thevalidator.dai.vinchik.matcher;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public class Account {
+
+    public static int getUserID() {
+        return 519324877;
+    }
+
+    public static String getToken() {
+        return "vk1.a.V9yBC20P7DNyRypOrsqbc9kApq"
+                + "KQSNOwtvTv4hfDDpGC6fPsragEWC1"
+                + "6KMDCjjDwsbPanBo2H1EvSGCGGKYA"
+                + "twrSocDQSswMsusiOg1GgnCGxDSt7"
+                + "43CCTQ_nCEJ9SPhXoxeZ1_73gJcZB"
+                + "6GB5aW4dTEZfIVMu1ohaBh-SHXToy"
+                + "SDsNO_gxmpoDe9sBbg8FYa1r6WO40"
+                + "Le7FNTicqexIAQ";
+    }
+    
+}
