57443598c5d51988d44bb82c2e9d424a4522341b
thevalidator <the.validator@yandex.ru>
2022-12-31 21:54:26 +0300

    Update Property.java

--- src/main/java/ru/thevalidator/daivinchikmatcher/property/Property.java
+++ src/main/java/ru/thevalidator/daivinchikmatcher/property/Property.java
@@ -1,16 +1,20 @@
 /*
  * Copyright (C) 2022 thevalidator
  */
-
 package ru.thevalidator.daivinchikmatcher.property;
 
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.List;
 
 /**
  * @author thevalidator <the.validator@yandex.ru>
  */
 public class Property {
-    
+
     public static final String PROP_FILE = "properties.json";
     private List<Account> accounts;
     private List<UserAgent> userAgents;
@@ -39,7 +43,7 @@ public class Property {
 
     public void setProxies(List<Proxy> proxies) {
         this.proxies = proxies;
-    }    
+    }
 
     public Delay getDelay() {
         return delay;
@@ -47,6 +51,25 @@ public class Property {
 
     public void setDelay(Delay delay) {
         this.delay = delay;
+    }
+
+    public static void saveToJson(Property p) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
+            writer.writeValue(Paths.get(PROP_FILE).toFile(), p);
+        } catch (IOException e) {
+        }
+    }
+    
+    public static Property readFromJson(String path) {
+        Property p = null;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            p = mapper.readValue(Paths.get(PROP_FILE).toFile(), Property.class);
+        } catch (IOException e) {
+        }
+        return p;
     }
 
 }
