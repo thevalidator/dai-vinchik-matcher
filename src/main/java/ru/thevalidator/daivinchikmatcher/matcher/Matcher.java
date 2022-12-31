37cf46c05424b17fd8ae1b64b22ff33fd39c0ac7
thevalidator <the.validator@yandex.ru>
2023-01-01 02:04:35 +0300

    Create Matcher.java

---
+++ src/main/java/ru/thevalidator/daivinchikmatcher/matcher/Matcher.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2022 thevalidator
+ */
+
+package ru.thevalidator.daivinchikmatcher.matcher;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public interface Matcher {
+    
+    boolean hasMatched();
+
+}
