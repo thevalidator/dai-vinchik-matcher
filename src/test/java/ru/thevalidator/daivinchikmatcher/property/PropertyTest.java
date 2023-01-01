2014c1859a7482f2911c806a6cca2a8fd0c743bd
thevalidator <the.validator@yandex.ru>
2023-01-01 12:54:14 +0300

    Update PropertyTest.java

--- src/test/java/ru/thevalidator/daivinchikmatcher/property/PropertyTest.java
+++ src/test/java/ru/thevalidator/daivinchikmatcher/property/PropertyTest.java
@@ -40,7 +40,6 @@ public class PropertyTest {
 
     @Test
     public void testReadFromJson() {
-        
         Property result = Property.readFromJson("src/resources/properties_test.json");
         assertEquals(2, result.getAccounts().size());
         assertEquals(2, result.getUserAgents().size());
@@ -49,7 +48,45 @@ public class PropertyTest {
         assertEquals(15, result.getDelay().getRandomAddedDelay());
         assertEquals("token_1", result.getAccounts().get(0).getToken());
         assertEquals("account 2", result.getAccounts().get(1).getName());
-        assertEquals("xxx.xxx.xxx.xxx", result.getProxies().get(0).getAdress());
+        assertEquals("123.100.189.192", result.getProxies().get(0).getAdress());
+        assertEquals(12345, result.getProxies().get(1).getPort());
+        assertEquals("useragent value 1", result.getUserAgents().get(0).getValue());
     }
+    
+//    @Test
+//    public void testSaveToJson() {
+//        Property prop = new Property();
+//        
+//        Account acc1 = new Account();
+//        acc1.setName("name 1");
+//        acc1.setId(1111111);
+//        acc1.setToken("token1");
+//        Account acc2 = new Account();
+//        acc2.setName("name 2");
+//        acc2.setId(22222222);
+//        acc2.setToken("token2");
+//        
+//        UserAgent ua1 = new UserAgent();
+//        ua1.setName("useragent name 1");
+//        ua1.setValue("useragent 1 value");
+//        UserAgent ua2 = new UserAgent();
+//        ua2.setName("useragent name 2");
+//        ua2.setValue("useragent 2 value");
+//        
+//        Proxy pr1 = new Proxy("123.100.189.192", 3456);
+//        Proxy pr2 = new Proxy("144.111.167.88", 12345);
+//        
+//        Delay delay = new Delay();
+//        delay.setBaseDelay(10);
+//        delay.setRandomAddedDelay(15);
+//        
+//        prop.setAccounts(Arrays.asList(acc1, acc2));
+//        prop.setUserAgents(Arrays.asList(ua1, ua2));
+//        prop.setProxies(Arrays.asList(pr1, pr2));
+//        prop.setDelay(delay);
+//        
+//        Property.saveToJson(prop);
+//        
+//    }
     
 }
