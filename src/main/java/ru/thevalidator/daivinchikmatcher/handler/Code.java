5e1e2dfad1c39c04f8452d8c6630de0a85014428
thevalidator <the.validator@yandex.ru>
2023-01-02 07:17:02 +0300

    Create Code.java

---
+++ src/main/java/ru/thevalidator/daivinchikmatcher/handler/Code.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+
+package ru.thevalidator.daivinchikmatcher.handler;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public class Code {
+
+    public static final int INCOMING_MESSAGE = 4;
+}
