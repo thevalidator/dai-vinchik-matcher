b407a9b328191e713bf5f5071f8d0390d380e405
thevalidator <the.validator@yandex.ru>
2023-01-02 07:17:18 +0300

    Update ResponseParser.java

--- src/main/java/ru/thevalidator/daivinchikmatcher/parser/ResponseParser.java
+++ src/main/java/ru/thevalidator/daivinchikmatcher/parser/ResponseParser.java
@@ -5,8 +5,11 @@
 package ru.thevalidator.daivinchikmatcher.parser;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 import ru.thevalidator.daivinchikmatcher.dto.LongPollServerResponse;
+import ru.thevalidator.daivinchikmatcher.dto.keyboard.Button;
 import ru.thevalidator.daivinchikmatcher.dto.keyboard.Keyboard;
 import ru.thevalidator.daivinchikmatcher.dto.keyboard.KeyboardRoot;
 
@@ -24,6 +27,10 @@ public class ResponseParser {
     
     public static LongPollServerResponse parseLonPollRespone(String response) throws JsonProcessingException {
         return mapper.readValue(response, LongPollServerResponse.class);
+    }
+    
+    public static List<Button> parseButtons(String data) throws JsonProcessingException {
+        return mapper.readValue(data, new TypeReference<List<Button>>(){});
     }
 
 }
