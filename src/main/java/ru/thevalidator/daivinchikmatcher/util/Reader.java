b0f2c7305cbc22ebe4b511e0abcb882bddaf714a
thevalidator <the.validator@yandex.ru>
2023-01-04 11:12:16 +0300

    Create Reader.java

---
+++ src/main/java/ru/thevalidator/daivinchikmatcher/util/Reader.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+
+package ru.thevalidator.daivinchikmatcher.util;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import ru.thevalidator.daivinchikmatcher.handler.impl.HandlerImpl;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public class Reader {
+    
+    public static Set<String> readDict(String path) {
+        Set<String> dict = new HashSet<>();
+        try ( BufferedReader br
+                = new BufferedReader(new InputStreamReader(new FileInputStream(path), "UTF-8"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                dict.add(line.trim());
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(HandlerImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return dict;
+    }
+
+}
