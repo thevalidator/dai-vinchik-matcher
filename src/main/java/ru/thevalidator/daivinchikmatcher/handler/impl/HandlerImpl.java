774409995c4149b6f34ce493cf15995904c6da13
thevalidator <the.validator@yandex.ru>
2023-01-05 20:22:19 +0300

    checks button labels first, thaen other cases

--- src/main/java/ru/thevalidator/daivinchikmatcher/handler/impl/HandlerImpl.java
+++ src/main/java/ru/thevalidator/daivinchikmatcher/handler/impl/HandlerImpl.java
@@ -6,9 +6,8 @@ package ru.thevalidator.daivinchikmatcher.handler.impl;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.thevalidator.daivinchikmatcher.util.FileUtil;
 import ru.thevalidator.daivinchikmatcher.dto.keyboard.Button;
 import ru.thevalidator.daivinchikmatcher.handler.Handler;
@@ -18,7 +17,7 @@ import ru.thevalidator.daivinchikmatcher.matcher.impl.FilterImpl;
 
 public class HandlerImpl implements Handler {
 
-    private static final org.apache.logging.log4j.Logger logger = LogManager.getLogger(HandlerImpl.class);
+    private static final Logger logger = LogManager.getLogger(HandlerImpl.class);
     private final Set<String> continueWords;
     private final Filter filter;
 
@@ -29,11 +28,25 @@ public class HandlerImpl implements Handler {
 
     @Override
     public String getAnswer(String messageText, List<Button> buttons) {
-        
+
         if (buttons == null) { //&& !messageText.contains("Нет такого варианта ответа, напиши одну цифру")) {
-            return null;
+            if (messageText.startsWith("Есть взаимная симпатия! Добавляй в друзья -")) {
+                return null;
+            } else if (messageText.startsWith("Нет такого варианта ответа")) {
+                System.out.print("ENTER CORRECT ANSWER:");
+                Scanner scanner = new Scanner(System.in);
+                String answer = scanner.nextLine();
+                //System.out.println("");
+
+                return "[CASE]-" + answer;
+            } else {
+                logger.error("[NO KEYBOARD MSG] - {}", messageText);
+                System.out.print("MESSAGE WITHOUT KEYBOARD, SKIPPING, CHECK IF BOT WORKS!!!");
+                return null;
+            }
+
         }
-        
+
         //checks continue words on buttons
         if (!buttons.isEmpty()) {
             for (Button b : buttons) {
@@ -49,51 +62,54 @@ public class HandlerImpl implements Handler {
                 }
             }
         }
-        
+
         if (isProfile(messageText, buttons)) {
-            System.out.println("[profile]");
+            //logger.info("[PROFILE] - {}", messageText);
+            System.out.println("[PROFILE] - " + messageText);
             if (filter.isMatched(messageText)) {
+                logger.info("[MATCH]");
                 return "1";
             } else {
+                logger.info("[NO MATCH]");
                 return "3";
             }
-            
+
         } else if (isExpired(messageText, buttons)) {
-            System.out.println("[expired]");
+            System.out.println("[EXPIRED]");
             return "2";
         } else if (isNeedSubscription(messageText, buttons)) {
-            System.out.println("[subscription]");
+            System.out.println("[SUBSCRIPTION]");
             return "2";
         } else if (isNoTextInProfileWarn(messageText, buttons)) {
-            System.out.println("no text my in profile warn");
+            System.out.println("[NO TEXT MY IN PROFILE WARN]");
             return "1";
         } else if (isLikedBySomeone(messageText, buttons)) {
-            System.out.println("liked by someone");
+            System.out.println("[LIKED BY SOMEONE]");
             return "1";
         } else if (isSleeping(messageText, buttons)) {
-            System.out.println("sleeping");
+            System.out.println("[SLEEPING]");
             return "1";
         } else if (isNewProfilesWantToMeet(messageText, buttons)) {
-            System.out.println("new profiles");
+            System.out.println("[NEW PROFILES]");
             return "1";
         } else if (isQuestion(messageText, buttons)) {
-            System.out.println("question");
+            System.out.println("[QUESTION]");
             return "2";
         } else if (isLocation(messageText, buttons)) {
-            System.out.println("location");
+            System.out.println("[LOCATION]");
             return "2";
         } else {
-            logger.error("unknown state");
-            System.out.println("[ERROR] - message:" + messageText + "\nbuttons: " + buttons.size() + "\t" + buttons.get(0).getColor());
-            System.out.print("\nENTER CORRECT ANSWER:");
+            logger.error("unknown state, need assistance");
+            //System.out.println("[ERROR] - message:" + messageText + "\nbuttons: " + buttons.size() + "\t" + buttons.get(0).getColor());
+            System.out.print("ENTER CORRECT ANSWER:");
             Scanner scanner = new Scanner(System.in);
             String answer = scanner.nextLine();
-            System.out.println("");
-            
+            //System.out.println("");
+
             return "[CASE]-" + answer;
             //throw new UnsupportedOperationException("Unknown state");
         }
-        
+
     }
 
 }
