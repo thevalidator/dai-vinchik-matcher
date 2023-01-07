c855a854a111877f3ebff50518ee54c93c2137b3
thevalidator <the.validator@yandex.ru>
2023-01-07 22:28:45 +0300

    refactored, added filrters

--- src/main/java/ru/thevalidator/daivinchikmatcher/handler/impl/HandlerImpl.java
+++ src/main/java/ru/thevalidator/daivinchikmatcher/handler/impl/HandlerImpl.java
@@ -3,6 +3,14 @@
  */
 package ru.thevalidator.daivinchikmatcher.handler.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.UserActor;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+import com.vk.api.sdk.objects.messages.KeyboardButton;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
@@ -10,43 +18,86 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ru.thevalidator.daivinchikmatcher.util.FileUtil;
 import ru.thevalidator.daivinchikmatcher.dto.keyboard.Button;
+import ru.thevalidator.daivinchikmatcher.dto.keyboard.Keyboard;
+import ru.thevalidator.daivinchikmatcher.handler.Code;
+import ru.thevalidator.daivinchikmatcher.handler.Flag;
 import ru.thevalidator.daivinchikmatcher.handler.Handler;
 import static ru.thevalidator.daivinchikmatcher.handler.Identifier.*;
 import ru.thevalidator.daivinchikmatcher.matcher.Filter;
-import ru.thevalidator.daivinchikmatcher.matcher.impl.FilterImpl;
+import ru.thevalidator.daivinchikmatcher.matcher.impl.ProfileMatchCheckerImpl;
+import ru.thevalidator.daivinchikmatcher.matcher.MatchChecker;
+import ru.thevalidator.daivinchikmatcher.parser.ResponseParser;
+import static ru.thevalidator.daivinchikmatcher.property.Data.DAI_VINCHIK_BOT_CHAT_ID;
+import ru.thevalidator.daivinchikmatcher.util.ExceptionUtil;
+import ru.thevalidator.daivinchikmatcher.util.VKUtil;
 
 public class HandlerImpl implements Handler {
 
     private static final Logger logger = LogManager.getLogger(HandlerImpl.class);
+    private static final int[] flags = Flag.getAllFlagCodes();
     private final Set<String> continueWords;
-    private final Filter filter;
+    private final MatchChecker checker;
+    private VkApiClient vk;
+    private UserActor actor;
 
-    public HandlerImpl() {
+    public HandlerImpl(Set<Filter> filters, VkApiClient vk, UserActor actor) {
         this.continueWords = FileUtil.readDict("continue.dict");
-        this.filter = new FilterImpl();
+        this.checker = new ProfileMatchCheckerImpl(filters);
+        this.vk = vk;
+        this.actor = actor;
     }
 
     @Override
-    public String getAnswer(String messageText, List<Button> buttons) {
+    public String getStartMessage(String lastMessageText, List<Button> buttons) {
+        return generateMessage(lastMessageText, buttons);
+    }
 
-        if (buttons == null) { //&& !messageText.contains("Нет такого варианта ответа, напиши одну цифру")) {
-            if (messageText.startsWith("Есть взаимная симпатия! Добавляй в друзья -")) {
-                return null;
-            } else if (messageText.startsWith("Нет такого варианта ответа")) {
-                System.out.print("ENTER CORRECT ANSWER:");
-                Scanner scanner = new Scanner(System.in);
-                String answer = scanner.nextLine();
-                //System.out.println("");
+    @Override
+    public String getAnswerMessage(List<List<Object>> updates) {
+        String answer = null;
+        for (List<Object> o : updates) {
+            Integer code = (Integer) o.get(0);
 
-                return "[CASE]-" + answer;
-            } else {
-                logger.error("[NO KEYBOARD MSG] - {}", messageText);
-                System.out.print("MESSAGE WITHOUT KEYBOARD, SKIPPING, CHECK IF BOT WORKS!!!");
-                return null;
+            if (code == Code.INCOMING_MESSAGE) {
+                Integer flagsSum = (Integer) o.get(2);
+                Set<Integer> messageFlags = getFlags(flagsSum);
+
+                if (!messageFlags.contains(Flag.OUTBOX.getFlagCode())) {
+                    Integer minorId = (Integer) o.get(3);
+
+                    if (minorId == DAI_VINCHIK_BOT_CHAT_ID) { //2_000_000_000  // > 
+                        //System.out.println("[INCOMING DAIVIN] - " + o.toString());                      //debug. delete in future
+                        String message = o.get(5).toString();
+                        Keyboard actualKeyboard = ResponseParser.parseKeyboard(o.get(6));
+                        List<Button> buttons = actualKeyboard != null
+                                ? actualKeyboard.getButtons().get(actualKeyboard.getButtons().size() - 1)
+                                : null;
+
+                        if (buttons == null) {
+                            if (message.startsWith("Есть взаимная симпатия! Добавляй в друзья -")) {
+                                System.out.println("\"[MUTUAL LIKE CASE] - " +  message);
+                                //logger.info("[MUTUAL LIKE CASE] - {}", message);
+                            } else if (message.startsWith("Нет такого варианта ответа")) {
+                                answer = getCustomAnswer();
+                                break;
+                            }
+                        } else {
+                            answer = generateMessage(message, buttons);
+                            break;
+                        }
+                    }
+
+                }
+
             }
-
         }
 
+        return answer;
+    }
+
+    private String generateMessage(String messageText, List<Button> buttons) {
+        String message = null;
+        
         //checks continue words on buttons
         if (!buttons.isEmpty()) {
             for (Button b : buttons) {
@@ -62,54 +113,110 @@ public class HandlerImpl implements Handler {
                 }
             }
         }
-
+        
         if (isProfile(messageText, buttons)) {
-            //logger.info("[PROFILE] - {}", messageText);
+            //logger.info("[PROFILE CASE] - {}", messageText);
             System.out.println("[PROFILE] - " + messageText);
-            if (filter.isMatched(messageText)) {
-                logger.info("[MATCH]");
+            if (checker.matches(messageText)) {
+                //logger.info("[MATCH]");
+                System.out.println("[MATCH]");
                 return "1";
             } else {
-                logger.info("[NO MATCH]");
+                //logger.info("[NO MATCH]");
+                System.out.println("[NO MATCH]");
                 return "3";
             }
-
         } else if (isExpired(messageText, buttons)) {
-            System.out.println("[EXPIRED]");
+            System.out.println("[EXPIRED CASE]");
             return "2";
         } else if (isNeedSubscription(messageText, buttons)) {
-            System.out.println("[SUBSCRIPTION]");
+            System.out.println("[NEED SUBSCRIPTION CASE]");
             return "2";
         } else if (isNoTextInProfileWarn(messageText, buttons)) {
-            System.out.println("[NO TEXT MY IN PROFILE WARN]");
+            System.out.println("[NO TEXT IN PROFILE WARN CASE]");
             return "1";
         } else if (isLikedBySomeone(messageText, buttons)) {
-            System.out.println("[LIKED BY SOMEONE]");
+            System.out.println("[LIKED BY SOMEONE CASE]");
             return "1";
         } else if (isSleeping(messageText, buttons)) {
-            System.out.println("[SLEEPING]");
+            System.out.println("[SLEEPING CASE]");
             return "1";
         } else if (isNewProfilesWantToMeet(messageText, buttons)) {
-            System.out.println("[NEW PROFILES]");
+            System.out.println("[NEW PROFILES CASE]");
             return "1";
         } else if (isQuestion(messageText, buttons)) {
-            System.out.println("[QUESTION]");
+            System.out.println("[QUESTION/ADVERTISEMENT CASE]");
             return "2";
         } else if (isLocation(messageText, buttons)) {
-            System.out.println("[LOCATION]");
-            return "2";
+            try {
+                System.out.println("[LOCATION CASE]");
+                //startSoundAlarm();
+                ObjectMapper mapper = new ObjectMapper();
+                String output = mapper.writeValueAsString(buttons);
+                System.out.println("[LOCATION DEBUG INFO]\nmessage=" + messageText + "\nbuttons=" + output);
+                //logger.info("- [LOCATION] - message={}, buttons{}", messageText, output);
+            } catch (JsonProcessingException ex) {
+                logger.error(ex.getMessage());
+            }
+            return "1";
         } else {
-            logger.error("unknown state, need assistance");
-            //System.out.println("[ERROR] - message:" + messageText + "\nbuttons: " + buttons.size() + "\t" + buttons.get(0).getColor());
-            System.out.print("ENTER CORRECT ANSWER:");
-            Scanner scanner = new Scanner(System.in);
-            String answer = scanner.nextLine();
-            //System.out.println("");
-
-            return "[CASE]-" + answer;
-            //throw new UnsupportedOperationException("Unknown state");
+            message = getCustomAnswer();
         }
 
+        return message;
+    }
+
+    public Set<Integer> getFlags(int sum) {
+        Set<Integer> result = new HashSet<>();
+        for (int flag : flags) {
+            if ((flag & sum) == flag) {
+                result.add(flag);
+            }
+        }
+
+        return result;
+    }
+
+    private void printButtons(List<List<KeyboardButton>> buttonRows) {
+        System.out.println("""
+                           **********************
+                           *       BUTTONS      *
+                           **********************
+                           """);
+        for (List<KeyboardButton> row : buttonRows) {
+            System.out.println(">>> NEW LINE");
+            for (KeyboardButton button : row) {
+                System.out.println("\t'PAYLOAD' = " + button.getAction().getPayload()
+                        + " 'LABEL' = " + button.getAction().getLabel());
+            }
+        }
+        System.out.println("**********************");
+    }
+
+    private String getCustomAnswer() {
+        String answer = null;
+        try {
+            var conversation = VKUtil.getConversation(vk, actor, DAI_VINCHIK_BOT_CHAT_ID);
+            
+            var keyboard = conversation.getCurrentKeyboard();
+            List<List<KeyboardButton>> buttonRows = keyboard.getButtons();
+            printButtons(buttonRows);
+            System.out.print("ENTER CORRECT ANSWER:");
+            startSoundAlarm();
+            Scanner scanner = new Scanner(System.in);
+            answer = scanner.nextLine();
+            while (answer == null || answer.isEmpty() || answer.isBlank()) {
+                System.out.print("ERROR: empty input, try again: ");
+                answer = scanner.nextLine();
+            }
+            String history = vk.messages().getHistory(actor).peerId(DAI_VINCHIK_BOT_CHAT_ID).count(5).executeAsString();
+            logger.error(" - UNKNOWN STATE: \nhistory={}, \nkeyboard={}, \nanswer={}", history, keyboard.toPrettyString(), answer);
+
+        } catch (ApiException | ClientException ex) {
+            logger.error(ExceptionUtil.getFormattedDescription(ex));
+        }
+
+        return answer;
     }
 
 }
