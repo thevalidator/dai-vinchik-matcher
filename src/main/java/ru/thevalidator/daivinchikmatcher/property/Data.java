0062c4eeb52e6254fc3c978b925fb8e0e3af5b68
thevalidator <the.validator@yandex.ru>
2023-01-07 22:28:49 +0300

    Create Data.java

---
+++ src/main/java/ru/thevalidator/daivinchikmatcher/property/Data.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+
+package ru.thevalidator.daivinchikmatcher.property;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public class Data {
+    
+    public static final int DAI_VINCHIK_BOT_CHAT_ID = -91050183;
+
+}
