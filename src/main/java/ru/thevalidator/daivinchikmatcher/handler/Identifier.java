6f25aae1fc2b42b17c6628c8e915fdff4a4200eb
thevalidator <the.validator@yandex.ru>
2023-01-12 00:58:32 +0300

    clean code and modify regexp

--- src/main/java/ru/thevalidator/daivinchikmatcher/handler/Identifier.java
+++ src/main/java/ru/thevalidator/daivinchikmatcher/handler/Identifier.java
@@ -5,30 +5,31 @@
 package ru.thevalidator.daivinchikmatcher.handler;
 
 import com.vdurmont.emoji.EmojiParser;
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.util.List;
-import javazoom.jl.player.Player;
 import ru.thevalidator.daivinchikmatcher.dto.keyboard.Button;
 
 /**
  * @author thevalidator <the.validator@yandex.ru>
  */
-public class Identifier {
-                                       //(.+(<br>|\\n){1,})?(?<name>(.+)?,){0,1} (?<age>\\d{1,3},) (?<city>[a-zA-Zа-яА-я0-9,\\.-ёЁ ]+)(?<text>(((<br>)|\\n){1,}.+){0,})
-    public static final String REGEXP = "(.+(<br>|\\n){1,})?(?<name>(.+)?,){0,1} (?<age>\\d{1,3},) (?<city>[a-zA-Zа-яА-я0-9,\\.\\-–ёЁ ]+)(?<text>(((<br>)|\\n){1,}.+){0,})";
-    //"(?<name>.+,) (?<age>\\d{1,3},) (?<city>[a-zA-Zа-яА-я0-9 \\-,\\.ёЁ]+)(?<text>(((<br>)|\\n){1,}.+){0,})";
-//"(?<name>.+,) (?<age>\\d{1,3},) (?<city>[a-zA-Zа-яА-я0-9 -,.]+)(?<text>(((<br>)|\\n).+){0,})";
+public class Identifier {             
     
+                                      //  \p{L} – to allow all letters from any language
+                                      //  \p{N} – for numbers
+                                      //  \p{P} – for punctuation
+                                      //  \p{Z} – for whitespace separators
+    
+                                      //  \\p{L}\\p{N}\\p{P}\\p{Z}
+                                      //"([\\p{L}\\p{N}\\p{P}\\p{Z}$^+=|><`~№]+(<br>|\\n){1,})?(?<name>([\\p{L}\\p{N}\\p{P}\\p{Z}$^+=|><`~№]+),){1} (?<age>\\d{1,3},){1} (?<city>[\\p{L}\\p{N}\\p{P}\\p{Z}$^+=|><`~№]+){1}(?<text>(((<br>)|\\n){1,}.{0,}){0,})";
+                                      //"(.+(<br>|\\n){1,})?(?<name>(.+)?,){1} (?<age>\\d{1,3},){1} (?<city>[a-zA-Zа-яА-я0-9,\\.\\-–ёЁ()? ]+){1}(?<text>(((<br>)|\\n){1,}.{0,}){0,})";
+                                      //"(.+(<br>|\\n){1,})?(?<name>(.+)?,){0,1} (?<age>\\d{1,3},) (?<city>[a-zA-Zа-яА-я0-9,\\.\\-–ёЁ()? ]+)(?<text>(((<br>)|\\n){1,}.{0,}){0,})";
+    public static final String REGEXP = "([\\p{L}\\p{N}\\p{P}\\p{Z}$\\^+=|`~№]+(<br>|\\n){1,})?(?<name>([\\p{L}\\p{N}\\p{P}\\p{Z}$\\^+=|`~№]+)?,){1} (?<age>\\d{1,3},){1} (?<city>[\\p{L}\\p{N}\\p{P}\\p{Z}$\\^+=|`~№]+){1}(?<text>(((<br>)|\\n){1,}.{0,}){0,})";
+   
     public static boolean isProfile(String messageText, List<Button> buttons) {
         String text = EmojiParser.removeAllEmojis(messageText);
         boolean result = false;
         if ((buttons != null) && (buttons.size() == 4)) {
             if (!text.isEmpty() 
                     && text.matches(REGEXP)
-                    //&& messageText.matches("^(?<name>.+,) (?<age>\\d{1,3},) (?<city>[a-zA-Zа-яА-я0-9 -]+)(?<text>(((<br>)|\\n).+){0,})")
-                    //&& messageText.matches("(.+, \\d{1,3}, .+){1}((<br>|\\n).+)?")  //TODO: fix regexp
                     && "positive".equals(buttons.get(0).getColor())
                     && "positive".equals(buttons.get(1).getColor())
                     && "negative".equals(buttons.get(2).getColor())
@@ -57,7 +58,7 @@ public class Identifier {
         return result;
     }
     
-    public static boolean isExpired(String messageText, List<Button> buttons) {
+    public static boolean isTelegramInvite(String messageText, List<Button> buttons) {
         boolean result = false;
         if ((buttons != null) && (buttons.size() == 2)) {
             if (!messageText.isEmpty() 
@@ -78,12 +79,7 @@ public class Identifier {
         boolean result = false;
         //if ((buttons != null) && (buttons.size() == 1)) {
             if (!messageText.isEmpty() 
-                    //&& messageText.contains("пришли мне свое местоположение и увидишь кто находится рядом")
                     && messageText.endsWith("пришли мне свое местоположение и увидишь кто находится рядом")) {
-                    //&& "default".equals(buttons.get(0).getColor())
-//                    && "Продолжить просмотр анкет".equals(buttons.get(0).getAction().getLabel())) {
-                
-                //startSoundAlarm();
                 
                 result = true;
             }
@@ -101,23 +97,6 @@ public class Identifier {
                     && "Продолжить".equals(buttons.get(0).getAction().getLabel())
                     && "default".equals(buttons.get(1).getColor())
                     && "Возможно позже".equals(buttons.get(1).getAction().getLabel())) {
-                
-                result = true;
-            }
-        }
-
-        return result;
-    }
-    
-    public static boolean isQuestion(String messageText, List<Button> buttons) {
-        boolean result = false;
-        if ((buttons != null) && (buttons.size() == 2)) {
-            if (!messageText.isEmpty() 
-                    //&& messageText.contains("Подождем пока кто-то увидит твоб анкету")
-                    && "positive".equals(buttons.get(0).getColor())
-                    && "Как это работает?".equals(buttons.get(0).getAction().getLabel())
-                    && "default".equals(buttons.get(1).getColor())
-                    && "Хороший совет".equals(buttons.get(1).getAction().getLabel())) {
                 
                 result = true;
             }
@@ -146,8 +125,7 @@ public class Identifier {
     public static boolean isSleeping(String messageText, List<Button> buttons) {
         boolean result = false;
         if ((buttons != null) && (buttons.size() == 4)) {
-            if (!messageText.isEmpty()     //1. Смотреть анкеты.<br>2. Моя анкета.<br>3. Я больше не хочу никого искать.<br>***<br>4. ✈️ Бот знакомств Дайвинчик в Telegram.
-                    //&& messageText.endsWith("1. Смотреть анкеты.<br>2. Моя анкета.<br>3. Я больше не хочу никого искать.<br>***<br>4. ?? Бот знакомств Дайвинчик в Telegram.")
+            if (!messageText.isEmpty()    
                     && messageText.startsWith("1. Смотреть анкеты.")
                     && messageText.endsWith("Бот знакомств Дайвинчик в Telegram.")
                     && "positive".equals(buttons.get(0).getColor())
@@ -193,37 +171,24 @@ public class Identifier {
         return result;
     }
     
-//    public static boolean isMutualLike(String messageText, List<Button> buttons) {
-//        boolean result = false;
-//        if ((buttons == null) || (buttons.isEmpty())) {
-//            if (!messageText.isEmpty() 
-//                    && messageText.startsWith("Есть взаимная симпатия! Добавляй в друзья")) {
-//                
-//                result = true;
-//            }
-//        }
-//
-//        return result;
-//    }
+    public static boolean isMutualLike(String messageText, List<Button> buttons) {
+        boolean result = false;
+        //if ((buttons == null) || (buttons.isEmpty())) {
+            if (!messageText.isEmpty() 
+                    && messageText.startsWith("Есть взаимная симпатия! Добавляй в друзья")) {
+                
+                result = true;
+            }
+        //}
+
+        return result;
+    }
     
     public static boolean isCaptcha(String messageText, List<Button> buttons) {
         boolean result = false;
         
 
         return result;
-    }
-    
-    public static void startSoundAlarm() {
-        String audioFilePath = "signal.mp3";
-        try {
-            BufferedInputStream buffer = new BufferedInputStream(
-                    new FileInputStream(new File(audioFilePath))
-            );
-            Player mp3Player = new Player(buffer);
-            mp3Player.play();
-        } catch (Exception ex) {
-            System.out.println("Error occured during playback process:" + ex.getMessage());
-        }
     }
 
 }
