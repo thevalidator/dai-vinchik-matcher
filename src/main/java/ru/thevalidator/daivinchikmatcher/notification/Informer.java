49e8c36c4535b65dd5d8c65a96d19f38bd042716
thevalidator <the.validator@yandex.ru>
2023-01-12 21:44:26 +0300

    Create Informer.java

---
+++ src/main/java/ru/thevalidator/daivinchikmatcher/notification/Informer.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+
+package ru.thevalidator.daivinchikmatcher.notification;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public class Informer {
+    
+    private final Object MONITOR = new Object();
+    private Set<Observer> observers;
+
+    public void registerObserver(Observer observer) {
+        if (observer == null) {
+            return;
+        }
+        synchronized (MONITOR) {
+            if (observers == null) {
+                observers = new HashSet<>(1);
+            }
+            observers.add(observer);
+        }
+    }
+
+    public void unregisterObserver(Observer observer) {
+        if (observer == null) {
+            return;
+        }
+        synchronized (MONITOR) {
+            observers.remove(observer);
+        }
+    }
+
+    public void informObservers(String message) {
+        Set<Observer> observersCopy;
+        synchronized (MONITOR) {
+            if (observers == null) {
+                return;
+            }
+            observersCopy = new HashSet<>(observers);
+        }
+        for (Observer observer : observersCopy) {
+            observer.onUpdateRecieve(message);
+        }
+    }
+    
+}
