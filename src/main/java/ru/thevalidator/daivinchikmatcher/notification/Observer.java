588fc4fb28ffcc3f32d6fb505d0d443bee388b9c
thevalidator <the.validator@yandex.ru>
2023-01-12 21:44:30 +0300

    Create Observer.java

---
+++ src/main/java/ru/thevalidator/daivinchikmatcher/notification/Observer.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+
+package ru.thevalidator.daivinchikmatcher.notification;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public interface Observer {
+
+    void onUpdateRecieve(String message);
+
+}
