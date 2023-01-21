3288116994ce64845a1cc60443656ac1c18efb52
thevalidator <the.validator@yandex.ru>
2023-01-21 16:58:41 +0300

    Create SettingsDTO.java

---
+++ src/main/java/ru/thevalidator/daivinchikmatcher/dto/settings/SettingsDTO.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+
+package ru.thevalidator.daivinchikmatcher.dto.settings;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public class SettingsDTO {
+    
+//    WINDOW_DIMENTIONS,
+//    LONG_POLL_DELAY,
+//    AGE_FILTER,
+//    CITY_FILTER,
+//    TEXT_FILTER,
+//    SOUND_ALARM,
+//    LIKE_ON_LIKE,
+//    EXPERIMENTAL_HANDLER,
+//    HOURS_TO_SLEEP,
+//    DEBUG_MODE
+    
+    @JsonProperty("window dimensions")
+    private int[] windowDimentions;
+    @JsonProperty("long poll delay")
+    private int longPollDelay;
+    
+    @JsonProperty("age filter")
+    private boolean hasAgeFilter;
+    @JsonProperty("city filter")
+    private boolean hasCityFilter;
+    @JsonProperty("text filter")
+    private boolean hasTextFilter;
+    
+    @JsonProperty("sound")
+    private boolean hasSoundAlarm;
+    @JsonProperty("like on like")
+    private boolean shouldLikeOnLike;
+    @JsonProperty("experimental functions")
+    private boolean hasExperimentalHandler;
+    
+    @JsonProperty("sleep hours")
+    private int hoursToSleep;
+    @JsonProperty("debug mode")
+    private boolean isDebugMode;
+
+    public int[] getWindowDimentions() {
+        return windowDimentions;
+    }
+
+    public void setWindowDimentions(int[] windowDimentions) {
+        this.windowDimentions = windowDimentions;
+    }
+
+    public int getLongPollDelay() {
+        return longPollDelay;
+    }
+
+    public void setLongPollDelay(int longPollDelay) {
+        this.longPollDelay = longPollDelay;
+    }
+
+    public boolean hasAgeFilter() {
+        return hasAgeFilter;
+    }
+
+    public void setAgeFilter(boolean ageFilter) {
+        this.hasAgeFilter = ageFilter;
+    }
+
+    public boolean hasCityFilter() {
+        return hasCityFilter;
+    }
+
+    public void setCityFilter(boolean cityFilter) {
+        this.hasCityFilter = cityFilter;
+    }
+
+    public boolean hasTextFilter() {
+        return hasTextFilter;
+    }
+
+    public void setTextFilter(boolean textFilter) {
+        this.hasTextFilter = textFilter;
+    }
+
+    public boolean hasSoundAlarm() {
+        return hasSoundAlarm;
+    }
+
+    public void setSoundAlarm(boolean soundAlarm) {
+        this.hasSoundAlarm = soundAlarm;
+    }
+
+    public boolean shouldLikeOnLike() {
+        return shouldLikeOnLike;
+    }
+
+    public void setLikeOnLike(boolean likeOnLike) {
+        this.shouldLikeOnLike = likeOnLike;
+    }
+
+    public boolean hasExperimentalHandler() {
+        return hasExperimentalHandler;
+    }
+
+    public void setExperimentalHandler(boolean experimentalHandler) {
+        this.hasExperimentalHandler = experimentalHandler;
+    }
+
+    public int getHoursToSleep() {
+        return hoursToSleep;
+    }
+
+    public void setHoursToSleep(int hoursToSleep) {
+        this.hoursToSleep = hoursToSleep;
+    }
+
+    public boolean isDebugMode() {
+        return isDebugMode;
+    }
+
+    @JsonIgnore
+    public void setDebugMode(boolean debugMode) {
+        this.isDebugMode = debugMode;
+    }
+
+    
+}
