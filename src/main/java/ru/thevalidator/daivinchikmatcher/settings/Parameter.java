fb618664ba67f69a3aef4fe1e9e24bb6d1d61aab
thevalidator <the.validator@yandex.ru>
2023-01-21 16:58:34 +0300

    Create Parameter.java

---
+++ src/main/java/ru/thevalidator/daivinchikmatcher/settings/Parameter.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+package ru.thevalidator.daivinchikmatcher.settings;
+
+/**
+ *
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public enum Parameter {
+    
+    WINDOW_DIMENTIONS,
+    LONG_POLL_DELAY,
+    AGE_FILTER,
+    CITY_FILTER,
+    TEXT_FILTER,
+    SOUND_ALARM,
+    LIKE_ON_LIKE,
+    EXPERIMENTAL_HANDLER,
+    HOURS_TO_SLEEP,
+    DEBUG_MODE
+    
+}
