13f41e2c0a44b0672bfd3251c1b62e4536360346
thevalidator <the.validator@yandex.ru>
2023-01-25 07:53:03 +0300

    refactored

--- src/main/java/ru/thevalidator/daivinchikmatcher/util/SoundUtil.java
+++ src/main/java/ru/thevalidator/daivinchikmatcher/util/SoundUtil.java
@@ -6,43 +6,37 @@ package ru.thevalidator.daivinchikmatcher.util;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import static ru.thevalidator.daivinchikmatcher.property.Data.ALERT_PATH;
+import static ru.thevalidator.daivinchikmatcher.property.Data.NOTIFICATION_PATH;
 
 /**
  * @author thevalidator <the.validator@yandex.ru>
  */
 public class SoundUtil {
-    
+
     private static final Logger logger = LogManager.getLogger(SoundUtil.class);
 
-    public static void startSoundAlarm() {
-        String audioFilePath = "media/signal.mp3";
-        try {
-            BufferedInputStream buffer = new BufferedInputStream(
-                    new FileInputStream(new File(audioFilePath))
-            );
-            Player mp3Player = new Player(buffer);
-            mp3Player.play();
-        } catch (FileNotFoundException | JavaLayerException ex) {
-            //System.out.println("Error occured during playback process:" + ex.getMessage());
-            logger.error(ExceptionUtil.getFormattedDescription(ex));
-        }
+    public static void playAlarm() {
+        playSound(ALERT_PATH);
     }
-    
-    public static void startSounNotification() {
-        String audioFilePath = "media/notification.mp3";
-        try {
-            BufferedInputStream buffer = new BufferedInputStream(
-                    new FileInputStream(new File(audioFilePath))
-            );
+
+    public static void playNotification() {
+        playSound(NOTIFICATION_PATH);
+    }
+
+    private static void playSound(String audioFilePath) {
+        try (FileInputStream fis = new FileInputStream(new File(audioFilePath));
+                BufferedInputStream buffer = new BufferedInputStream(fis)) {
+            
             Player mp3Player = new Player(buffer);
             mp3Player.play();
-        } catch (FileNotFoundException | JavaLayerException ex) {
-            //System.out.println("Error occured during playback process:" + ex.getMessage());
+            
+        } catch (IOException | JavaLayerException ex) {
             logger.error(ExceptionUtil.getFormattedDescription(ex));
         }
     }
