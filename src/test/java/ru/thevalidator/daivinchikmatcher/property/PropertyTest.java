070246da129898eecddf6e176303855df8e7cec9
thevalidator <the.validator@yandex.ru>
2023-01-25 08:00:27 +0300

    Update PropertyTest.java

--- src/test/java/ru/thevalidator/daivinchikmatcher/property/PropertyTest.java
+++ src/test/java/ru/thevalidator/daivinchikmatcher/property/PropertyTest.java
@@ -3,9 +3,6 @@
  */
 package ru.thevalidator.daivinchikmatcher.property;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -44,8 +41,6 @@ public class PropertyTest {
         assertEquals(2, result.getAccounts().size());
         assertEquals(2, result.getUserAgents().size());
         assertEquals(2, result.getProxies().size());
-        assertEquals(10, result.getDelay().getBaseDelay());
-        assertEquals(15, result.getDelay().getRandomAddedDelay());
         assertEquals("token_1", result.getAccounts().get(0).getToken());
         assertEquals("account 2", result.getAccounts().get(1).getName());
         assertEquals("123.100.189.192", result.getProxies().get(0).getAdress());
