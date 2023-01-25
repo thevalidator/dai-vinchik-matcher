7ec2c78fd56b3085c58480e774a5be94330e4ebe
thevalidator <the.validator@yandex.ru>
2023-01-25 07:58:16 +0300

    added notification audio file path

--- src/main/java/ru/thevalidator/daivinchikmatcher/property/Data.java
+++ src/main/java/ru/thevalidator/daivinchikmatcher/property/Data.java
@@ -13,6 +13,8 @@ public class Data {
     
     public static final String ALERT_PATH = "media/signal.mp3";
     
+    public static final String NOTIFICATION_PATH = "media/notification.mp3";
+    
     public static final String CONTINUE_WORDS = "config/continue.dict";
     
     public static final String AGES = "config/ages.dict";
